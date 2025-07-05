81733966250c239bc5d63555092d6919bebb3e6c
Akshanabha Chakraborty <ahqsecret@gmail.com>
2025-07-05 21:21:19 +0530

    add update implementation, not yet ready

--- src/ahqdb/mod.rs
+++ src/ahqdb/mod.rs
@@ -4,11 +4,14 @@ use std::path::Path;
 use std::{fs::File, io::Read};
 
 use serde::{Deserialize, Serialize};
+use serde_xml_rs::to_string;
 use tokio::task::spawn_blocking;
+use toml::from_str;
 use zip::ZipArchive;
 
 use crate::ahqdb::ps1::install::{run_install_ps1, run_uninstall_ps1};
 use crate::ahqdb::ps1::run_is_installed_ps1;
+use crate::ahqdb::ps1::update::run_update_ps1;
 use crate::utils::is_admin;
 use crate::zip::link::{link, unlink, ShortcutCreationInfo, Type};
 use crate::zip::ZipShortcut;
@@ -96,32 +99,31 @@ impl<'a> AHQDBApplication<'a> {
     let path = path.as_ref();
     let file = File::open(path)?;
 
-    let archive = ZipArchive::new(file)?;
+    let mut archive = ZipArchive::new(file)?;
 
-    let mut out = Self {
+    let shortcut_info = Self::verify(&mut archive)?;
+
+    let out = Self {
       app_id,
       file: archive,
       shortcut,
-      shortcut_info: None,
+      shortcut_info: Some(shortcut_info),
       version,
     };
-
-    // Populate shortcut_info
-    out.shortcut_info = Some(out.verify()?);
 
     Ok(out)
   }
 
-  fn verify(&mut self) -> Result<ADBShortcut, AHQDBError> {
-    let install = self.file.by_name("install.ps1")?.is_file();
-    let uninstall = self.file.by_name("uninstall.ps1")?.is_file();
-    let update = self.file.by_name("update.ps1")?.is_file();
-    let is_installed = self.file.by_name("isInstalled.ps1")?.is_file();
-    let _build = self.file.by_name(".build")?.is_file();
+  fn verify(file: &mut ZipArchive<File>) -> Result<ADBShortcut, AHQDBError> {
+    let install = file.by_name("install.ps1")?.is_file();
+    let uninstall = file.by_name("uninstall.ps1")?.is_file();
+    let update = file.by_name("update.ps1")?.is_file();
+    let is_installed = file.by_name("isInstalled.ps1")?.is_file();
+    let _build = file.by_name(".build")?.is_file();
 
-    let dist = self.file.by_name("dist/")?.is_dir();
+    let dist = file.by_name("dist/")?.is_dir();
 
-    let mut link_data = self.file.by_name("link.toml")?;
+    let mut link_data = file.by_name("link.toml")?;
     let link = link_data.is_file();
 
     if install && uninstall && update && is_installed && link && dist {
@@ -171,20 +173,30 @@ impl<'a> AHQDBApplication<'a> {
     run_is_installed_ps1(&script, &dist, &ty)
   }
 
+  fn get_type<T: AsRef<str>>(&self, dir: T) -> Result<Type, AHQDBError> {
+    let dir = dir.as_ref();
+
+    let r#type = fs::read_to_string(format!(r"{dir}\ahqdb\type"))?;
+    match r#type.as_str() {
+      "all" => Ok(Type::AllUsers),
+      "current" => Ok(Type::CurrentUser),
+      _ => Err(AHQDBError::InvalidAHQDBFile)
+    }
+  }
+
   /// ## SAFETY:
   /// This function uses `unsafe` and strict caller discipline to avoid undefined behavior
   /// The function must be awaited and resolved before self goes out of scope
   pub async fn async_uninstall(
     self,
     dir: String,
-    ty: Type
   ) -> Result<Self, AHQDBError> {
     let data: AHQDBApplication<'static> = unsafe { transmute(self) };
 
     spawn_blocking(move || {
       let mut data = data;
 
-      data.uninstall(dir, ty)?;
+      data.uninstall(dir)?;
 
       Ok(data)
     }).await?
@@ -193,8 +205,9 @@ impl<'a> AHQDBApplication<'a> {
   pub fn uninstall<T: AsRef<str>>(
     &mut self,
     dir: T,
-    ty: Type,
   ) -> Result<(), AHQDBError> {
+    let ty = self.get_type(&dir)?;
+
     if let Type::AllUsers = ty {
       if !is_admin().unwrap_or(false) {
         return Err(AHQDBError::NotElevated);
@@ -243,6 +256,136 @@ impl<'a> AHQDBApplication<'a> {
     Ok(())
   }
 
+
+  /// ## SAFETY:
+  /// This function uses `unsafe` and strict caller discipline to avoid undefined behavior
+  /// The function must be awaited and resolved before self goes out of scope
+  pub async fn async_update(
+    self,
+    dir: String
+  ) -> Result<(), AHQDBError> {
+    let mut me: AHQDBApplication<'static> = unsafe { transmute(self) };
+
+    spawn_blocking(move || {
+      me.update(dir)
+    }).await?
+  }
+
+  pub fn update<T: AsRef<str>>(
+    &mut self,
+    dir: T,
+  ) -> Result<(), AHQDBError> {
+    let ty = self.get_type(&dir)?;
+
+    let dir = dir.as_ref();
+
+    let old_ver = fs::read_to_string(format!(r"{dir}\ahqdb\.version"))?;
+
+    let dist_old = format!(r"{dir}\dist_{old_ver}");
+
+    let old_link: ADBShortcut = from_str(&fs::read_to_string(format!(r"{dir}\ahqdb\.link"))?)?;
+    let old_build = fs::read_to_string(format!(r"{dir}\ahqdb\.build"))?;
+
+    // Safety
+    // it'll never ever panic, guaranteed by the [`Self::new`] function
+    let ADBShortcut {
+      args,
+      description,
+      exe,
+      icon,
+      ignore,
+      name
+    } = &old_link;
+
+    // Remove the old shortcut
+    if !ignore.unwrap_or(false) {
+      let shortcut = ZipShortcut {
+        args: args.as_deref(),
+        description: description.as_deref(),
+        exe: exe.as_str(),
+        desktop: self.shortcut.desktop,
+        start_menu_dir: self.shortcut.start_menu_folder.as_deref(),
+        icon: icon.as_ref().map(|(string, num)| 
+          (string.as_ref(), *num)
+        ),
+        name: name.as_str()
+      };
+
+      unlink(&shortcut, self.app_id, ty.clone())?;
+    }
+
+
+
+    let ahqdb_new = format!(r"{dir}\ahqdb_new");
+
+    fs::create_dir_all(&ahqdb_new)?;
+
+    self.file.extract(&ahqdb_new)?;
+
+    let ahqdb_dist = format!(r"{dir}\ahqdb_new\dist");
+
+    let dist_final = format!(r"{dir}\dist_{}", self.version);
+    fs::create_dir_all(&dist_final)?;
+
+    copy_dir_all(&ahqdb_dist, &dist_final)?;
+
+    run_update_ps1(
+      "../ahqdb_new/update.ps1", 
+      &dist_old, 
+      &dist_final, 
+      &old_build, 
+      &ty
+    )?;
+
+    fs::remove_dir_all(dist_old)?;
+    fs::remove_dir_all(format!(r"{dir}\ahqdb"))?;
+    fs::rename(ahqdb_new, format!(r"{dir}\ahqdb"))?;
+
+    // Add Type
+    fs::write(format!(r"{dir}\ahqdb\type"), match ty {
+      Type::AllUsers => "all",
+      Type::CurrentUser => "current"
+    })?;
+
+    // Add Version
+    fs::write(format!(r"{dir}\ahqdb\.version"), self.version)?;
+
+    // Guaranteed that to_string won't error out
+    fs::write(format!(r"{dir}\ahqdb\.link"), to_string(self.shortcut_info.as_ref().unwrap()).map_err(|_| AHQDBError::InvalidAHQDBFile)?)?;
+
+
+    // Create the new shortcut
+    //
+    // Safety
+    // it'll never ever panic, guaranteed by the [`Self::new`] function
+    let ADBShortcut {
+      args,
+      description,
+      exe,
+      icon,
+      ignore,
+      name
+    } = self.shortcut_info.as_ref().unwrap();
+
+    if !ignore.unwrap_or(false) {
+      let shortcut = ZipShortcut {
+        args: args.as_deref(),
+        description: description.as_deref(),
+        exe: exe.as_str(),
+        desktop: self.shortcut.desktop,
+        start_menu_dir: self.shortcut.start_menu_folder.as_deref(),
+        icon: icon.as_ref().map(|(string, num)| 
+          (string.as_ref(), *num)
+        ),
+        name: name.as_str()
+      };
+
+      link(&shortcut, &dist_final, self.app_id, ty)?;
+    }
+
+    todo!()
+  }
+
   /// ## SAFETY:
   /// This function uses `unsafe` and strict caller discipline to avoid undefined behavior
   /// The function must be awaited and resolved before self goes out of scope
@@ -281,6 +424,18 @@ impl<'a> AHQDBApplication<'a> {
     fs::create_dir_all(&dist_final)?;
 
     self.file.extract(format!(r"{dir}\ahqdb"))?;
+
+    // Add Type
+    fs::write(format!(r"{dir}\ahqdb\type"), match ty {
+      Type::AllUsers => "all",
+      Type::CurrentUser => "current"
+    })?;
+
+    // Add Version
+    fs::write(format!(r"{dir}\ahqdb\.version"), self.version)?;
+
+    // Guaranteed that to_string won't error out
+    fs::write(format!(r"{dir}\ahqdb\.link"), to_string(self.shortcut_info.as_ref().unwrap()).map_err(|_| AHQDBError::InvalidAHQDBFile)?)?;
 
     let ahqdb_dist = format!(r"{dir}\ahqdb\dist");
     copy_dir_all(&ahqdb_dist, &dist_final)?;
